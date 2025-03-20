1fdea7df590ee0143ed5296a1c6bd8b04a6c87c4
jmathtech <jamil.matheny@gmail.com>
2025-03-20 05:58:22 +0000

    Added maxDuration to increase gateway timeout

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -5,6 +5,11 @@ import jwt from "jsonwebtoken";
 import validator from "validator";
 import { RowDataPacket } from "mysql2";
 
+export const config = {
+  maxDuration: 900,
+
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Only POST requests are allowed." });

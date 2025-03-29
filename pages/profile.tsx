d67b5fb8e3b6c46ce98dcee36723915f8e1c3f6c
jmathtech <jamil.matheny@gmail.com>
2025-03-29 19:52:59 +0000

    Updating manage profile section

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -106,7 +106,7 @@ const Profile = () => {
     <div className="bg-gray min-h-screen">
       <Navbar />
 
-      <main className="min-h-screen text-md mx-auto px-4 py-6">
+      <main className="container min-h-screen text-md mx-auto px-4 py-6">
         <div className="bg-white p-8 rounded shadow-xl my-6">
           <h2 className="text-lg text-gray-600 font-bold mt-8">Profile Details</h2>
           <div className="mt-4">

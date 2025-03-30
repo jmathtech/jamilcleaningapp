e2c6a33a0bbfec598125f059bfa83dc8a1492712
jmathtech <jamil.matheny@gmail.com>
2025-03-30 01:04:31 +0000

    Updating dashboard booking table..

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -544,7 +544,7 @@ const Dashboard = () => {
 
       {/* Dashboard Section */}
       <div className="bg-gray min-h-screen">
-        <main className="container mx-auto py-12">
+        <main className="container mx-auto max-w-4xl py-12">
           <h1 className="text-4xl text-gray-600 font-bold mb-6 mt-10 px-2">Dashboard</h1>
           <div
             className={`bg-white p-6 rounded shadow border-[#8ab13c] border mb-6 fade-in-page ${pageLoaded ? "loaded" : ""
@@ -654,7 +654,7 @@ const Dashboard = () => {
           </div>
 
           {/* Quick Links Section */}
-          <div className="bg-white p-6 rounded shadow border-[#8ab13c] border">
+          <div className="bg-white p-6 rounded shadow border-[#8ab13c] border mx-auto max-w-4xl">
             <h3 className="text-lg text-gray-600 font-bold mb-2">
               Quick Links
             </h3>

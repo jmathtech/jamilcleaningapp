22471d63537f8d7974194610c248979927c2778c
jmathtech <jamil.matheny@gmail.com>
2025-03-30 00:17:25 +0000

    Updating dashboard booking table..

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -316,13 +316,12 @@ const Dashboard = () => {
     expandedNotes: { [key: string]: boolean };
   }) => (
     <div className="overflow-x-auto rounded">
-      <table className="w-full border">
+      <table className="container border">
         <thead>
           <tr className="text-sm">
             {[
               "ID #",
-              "Booking Details",
-              "Actions"
+              "Booking Details"
             ].map((header) => (
               <th
                 key={header}
@@ -336,7 +335,7 @@ const Dashboard = () => {
         <tbody>
           {bookings.map((booking) => (
             <tr key={booking.booking_id}>
-              <td className="border px-3 py-4 text-lg">{booking.booking_id}</td>
+              <td className="border px-4 py-4 text-lg">{booking.booking_id}</td>
               <td className="border px-4 py-4 text-sm">
                 <i className="fa fa-calendar" aria-hidden="true" style={{ marginRight: '8px' }}></i> {new Date(booking.date).toISOString().split('T')[0]} <br></br>
                 <i className="fa fa-clock-o" aria-hidden="true" style={{ marginRight: '8px' }}></i> {formatTime(booking.time)} <br></br>
@@ -364,14 +363,13 @@ const Dashboard = () => {
                       </div>
                     )
                   ) : (
-                   <p>No notes</p> 
+                    <p>No notes</p>
                   )} </div>
                 <i className="fa fa-book" aria-hidden="true" style={{ marginRight: '8px' }}></i> {booking.service_type} <br></br>
                 <i className="fa fa-paw" aria-hidden="true" style={{ marginRight: '8px' }}></i> {booking.has_pets ? "Has Pets" : "No Pets"} <br></br>
                 <i className="fa fa-tasks" aria-hidden="true" style={{ marginRight: '8px' }}></i> Status: {booking.status} <br></br>
                 <i className="fa fa-money" aria-hidden="true" style={{ marginRight: '8px' }}></i> Total:  ${booking.total_price} <br></br>
-              </td>
-              <td className="border p-2">
+
                 <div className="flex flex-wrap gap-2">
                   <button
                     className="bg-[#3498db] transition-opacity duration-500 text-xs hover:opacity-80 hover:bg-[#85c1e9] text-white font-bold py-1 px-3 rounded-full flex items-center justify-center md:justify-normal md:space-x-2"

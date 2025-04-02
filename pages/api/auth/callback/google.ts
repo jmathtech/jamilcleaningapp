a47dde3c94b8412d835e4c30488bbae9b72699c4
jmathtech <jamil.matheny@gmail.com>
2025-04-02 23:57:08 +0000

    Adding other properties to the callback

--- pages/api/auth/callback/google.ts
+++ pages/api/auth/callback/google.ts
@@ -123,7 +123,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             res.setHeader('Set-Cookie', serialized);
 
             // Redirect to /api/verify with the token
-            res.redirect(`/dashboard`);
+            res.redirect(`/api/verify?token=${token}`);
         } else {
             // User not found in your database
             return res.status(401).json({ message: 'User not found. Please sign up with this email first.' });

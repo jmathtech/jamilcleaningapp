7278027d53d12d97d502f5e0d399076a0ef92256
jmathtech <jamil.matheny@gmail.com>
2025-04-03 20:22:20 +0000

    Updating the margin at footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#333] text-white font-semibold text-sm p-6 mt">
       <div className="container mx-auto text-center">
-        <div className="flex justify-center items-center payment-icons-container">
+        <div className="flex justify-center items-center mb-6 payment-icons-container">
           <Image src="/img/credit_cards/amex.svg" alt="Amex" width={75} height={55} style={{ maxWidth: "75px" }}  className="payment-icon mx" />
           <Image src="/img/credit_cards/mastercard.svg" alt="Mastercard" width={75} height={55} style={{ maxWidth: "75px" }}  className="payment-icon mx" />
           <Image src="/img/credit_cards/visa.svg" alt="Visa" width={75} height={55} style={{ maxWidth: "75px" }}  className="payment-icon mx" />
@@ -25,7 +25,7 @@ const Footer = () => {
           <Image src="/img/payment_options/klarna.svg" alt="Klarna" width={75} height={55} style={{ maxWidth: "75px" }}  className="payment-icon mx" />
         </div>
         <p>&copy; {new Date().getFullYear()}  Majestik Magik. All rights reserved.</p>
-        <div className="mt-6">
+        <div className="mt-2">
           <Link href="/privacy-policy" className="text-white font-semibold hover:text-[#C5D89D] px-2">Privacy Policy</Link>
           <Link href="/terms" className="text-white font-semibold hover:text-[#C5D89D] px-2">Terms of Service</Link>
         </div>

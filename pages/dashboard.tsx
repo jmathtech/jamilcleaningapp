941922286bc9a8310844876618f1005244f568a1
jmathtech <jamil.matheny@gmail.com>
2025-04-06 18:07:16 +0000

    Fix on the modals in the dashboard to match the backgroundColor rgba

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -336,7 +336,7 @@ const Dashboard = () => {
           {bookings.map((booking) => (
             <tr key={booking.booking_id}>
               <td className="border px-4 py-4 text-lg max-w-[75px]">{booking.booking_id}</td>
-              <td className="border px-4 py-4 text-sm">
+              <td className="border px-4 py-8 text-sm">
                 <i className="fa fa-calendar" aria-hidden="true" style={{ marginRight: '8px' }}></i> {new Date(booking.date).toISOString().split('T')[0]} <br></br>
                 <i className="fa fa-clock-o" aria-hidden="true" style={{ marginRight: '8px' }}></i> {formatTime(booking.time)} <br></br>
                 <i className="fa fa-hourglass-half" aria-hidden="true" style={{ marginRight: '8px' }}></i> {booking.hours} hours <br></br>

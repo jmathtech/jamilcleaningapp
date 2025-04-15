2d1c28aaccb6d85bc2b7b8dfba6f031d163362f5
jmathtech <jamil.matheny@gmail.com>
2025-04-15 19:49:50 +0000

    Adding effects to the menu nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,61 +14,65 @@ const Home = () => {
   </Head>
 
   return (
-    <div className="bg-gray index-container">
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="video-background"
-      >
-        <source src="/img/mmcleaning_crew_video_002.webm" type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
+    <>
       <Navbar />
 
-      <div className="flex justify-center items-center min-h-screen px-4">
-        <div className="bg-white mt-10 p-10 rounded-lg shadow w-full border-[#8ab13c] border max-w-xl text-center"
+      <div className="bg-gray index-container relative min-h-screen">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="video-background"
+        >
+          <source src="/img/mmcleaning_crew_video_002.webm" type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+
+
+        <div className="flex justify-center items-center min-h-screen px-4 relative z-10">
+          <div className="bg-white mt-10 p-10 rounded-lg shadow w-full border-[#8ab13c] border max-w-lg text-center"
           >
-          <h1 className="flex justify-center items-center p-6">
-            <Image
-              src="/img/majestik_magik_cleaning_01.png"
-              alt="Majestik Magik Cleaning"
-              width={250}
-              height={200}
-              priority={true} // Load image immediately
-              style={{ minWidth: "150px", maxWidth: "250px" }}
-            />
-          </h1>
-          <p className="font-semibold text-2xl text-[#8ab13c] mt-2">
-            HOUSE CLEANING SERVICES</p>
-          <p className="font-semibold text-xl text-gray-600 mt-2">
-            Prices start at $30 for a quality cleaning.</p>
-          <p className="text-gray-600 mt-2">Let&apos;s bring cleanliness & comfort to your space.</p>
-          <p className="text-lg p-8">
-            <Link
-              href="/signup"
-              className="text-[#8ab13c] font-bold hover:text-[#C5D89D]"
+            <h1 className="flex justify-center items-center p-6">
+              <Image
+                src="/img/majestik_magik_cleaning_01.png"
+                alt="Majestik Magik Cleaning"
+                width={250}
+                height={200}
+                priority={true} // Load image immediately
+                style={{ minWidth: "150px", maxWidth: "250px" }}
+              />
+            </h1>
+            <p className="font-semibold text-2xl text-[#8ab13c] mt-2">
+              HOUSE CLEANING SERVICES</p>
+            <p className="font-semibold text-xl text-gray-600 mt-2">
+              Prices start at $30 for a quality cleaning.</p>
+            <p className="text-gray-600 mt-2">Let&apos;s bring cleanliness & comfort to your space.</p>
+            <p className="text-lg p-8">
+              <Link
+                href="/signup"
+                className="text-[#8ab13c] font-bold hover:text-[#C5D89D]"
 
-            >
-              <button className="bg-[#8ab13c] transition-opacity duration-1000 text-md hover:opacity-80 hover:bg-[#9bbe56] text-white font-bold py-2 px-8 rounded mx-4 mb-2" style={{ width: "auto", height: "auto" }}>
-                Sign Up Today!
-              </button>
-            </Link>
-            <Link
-              href="/login"
-              className="text-[#8ab13c] font-bold hover:text-[#C5D89D]"
-            >
-              <button className="bg-[#8ab13c] transition-opacity duration-1000 text-md hover:opacity-80 hover:bg-[#9bbe56] text-white font-bold py-2 px-10 rounded mx-4 mb-2" style={{ width: "auto", height: "auto" }}>
-                Log In
-              </button>
-            </Link>
-          </p>
+              >
+                <button className="bg-[#8ab13c] transition-opacity duration-1000 text-md hover:opacity-80 hover:bg-[#9bbe56] text-white font-bold py-2 px-8 rounded mx-4 mb-2" style={{ width: "auto", height: "auto" }}>
+                  Sign Up Today!
+                </button>
+              </Link>
+              <Link
+                href="/login"
+                className="text-[#8ab13c] font-bold hover:text-[#C5D89D]"
+              >
+                <button className="bg-[#8ab13c] transition-opacity duration-1000 text-md hover:opacity-80 hover:bg-[#9bbe56] text-white font-bold py-2 px-10 rounded mx-4 mb-2" style={{ width: "auto", height: "auto" }}>
+                  Log In
+                </button>
+              </Link>
+            </p>
+          </div>
         </div>
-      </div>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
